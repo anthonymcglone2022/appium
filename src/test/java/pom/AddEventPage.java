44b2d4aa9e5513a1d9aea2fa3ecaed65cb5c5c65
Anthony McGlone <anthonymcglone2022@gmail.com>
2024-02-09 14:48:01 +0000

    Smoke Flow complete

---
+++ src/test/java/pom/AddEventPage.java
@@ -0,0 +1,102 @@
+package pom;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+
+public class AddEventPage {
+
+	AndroidDriver driver;
+	
+	public AddEventPage(AndroidDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(new AppiumFieldDecorator(driver),this);
+	}
+	
+	@AndroidFindBy(id = "com.testerzy.eduittraining:id/add_btn")
+	private WebElement addButton;
+	
+	@AndroidFindBy(id = "com.testerzy.eduittraining:id/save_btn")
+	private WebElement saveButton;
+
+	@AndroidFindBy(id = "com.testerzy.eduittraining:id/event_type")
+	private WebElement eventTypeDropDown;
+
+	@AndroidFindBy(id = "com.testerzy.eduittraining:id/event_edittext")
+	private WebElement eventNameEditText;
+
+	@AndroidFindBy(id = "com.testerzy.eduittraining:id/date_of_event_edittext")
+	private WebElement dateOfEventEditText;	
+	
+	@AndroidFindBy(id = "android:id/button1")
+	private WebElement popUpOkButton;	
+	
+	@AndroidFindBy(id = "com.testerzy.eduittraining:id/date_end_edittext")
+	private WebElement endOfEventEditText;	
+		
+	@AndroidFindBy(id = "com.testerzy.eduittraining:id/event_start_time_edittext")
+	private WebElement timeOfEventEditText;		
+	
+	@AndroidFindBy(id = "com.testerzy.eduittraining:id/event_end_time_edittext")
+	private WebElement endTimeOfEventEditText;	
+	
+	@AndroidFindBy(id = "com.testerzy.eduittraining:id/save_button")
+	private WebElement addEventPageSaveButton;	
+
+	public void clickAddButton(){
+		addButton.click();
+    }
+	
+	public void clickSaveButton(){
+		saveButton.click();
+    }	
+
+	public void clickEventTypeDropDown(){
+		eventTypeDropDown.click();
+    }
+	
+	public void clickEventTypeDropDownListSelection(String choice){
+		String xpath = "//android.widget.TextView[@resource-id=\"com.testerzy.eduittraining:id/event_type\" and @text=\"" + choice + "\"]";
+        driver.findElement(By.xpath(xpath)).click();
+    }	
+	
+	public void sendKeysEventNameEditText(String text){
+		eventNameEditText.sendKeys(text);
+    }	
+
+	public void sendKeysDateOfEventEditText(String text){
+		dateOfEventEditText.sendKeys(text);
+    }		
+
+	public void clickDateOfEventEditText(){
+		dateOfEventEditText.click();
+    }
+	
+	public void clickPopUpOkButton(){
+		popUpOkButton.click();
+	}
+
+	public void sendKeysEndOfEventEditText(String text){
+		endOfEventEditText.sendKeys(text);
+    }				
+	
+	public void sendKeysTimeOfEventEditText(String text){
+		timeOfEventEditText.sendKeys(text);
+    }		
+
+	public void clickTimeOfEventEditText(){
+		timeOfEventEditText.click();
+    }	
+
+	public void sendKeysEndTimeOfEventEditText(String text){
+		endTimeOfEventEditText.sendKeys(text);
+    }		
+
+	public void clickAddEventPageSaveButton(){
+		addEventPageSaveButton.click();
+	}
+}
--- src/test/java/tests/AppiumTest.java
+++ src/test/java/tests/AppiumTest.java
@@ -18,18 +18,64 @@ public class AppiumTest extends Driver {
 
 	@Test
 	public void smokeFlow() throws MalformedURLException, InterruptedException {		
+
 		Thread.sleep(2000);
+		
 		StartPage sP = new StartPage(driver);
 		sP.clickNextButton();
+		
 		Thread.sleep(2000);
 		
 		LoginPage lP = new LoginPage(driver);
 		lP.clickSkipButton();
+		
 		Thread.sleep(2000);
 
-		Operations.swipeLeft(driver);
+		Operations.swipeRight(driver);
 		
+		Thread.sleep(2000);
+		
+		AddEventPage aP = new AddEventPage(driver);
+		
+		aP.clickAddButton();
+		Thread.sleep(1000);
+		
+		aP.clickEventTypeDropDown();
+		Thread.sleep(1000);
+		
+		aP.clickEventTypeDropDownListSelection("Exam");
+		Thread.sleep(1000);
+		
+		aP.sendKeysEventNameEditText(String.format("Event Number %d", System.currentTimeMillis()));
+		Thread.sleep(1000);
+		
+		aP.sendKeysDateOfEventEditText("11.02.2026");
+		Thread.sleep(1000);
+		
+		aP.clickDateOfEventEditText();
+		Thread.sleep(1000);
+		
+		aP.clickPopUpOkButton();
+		Thread.sleep(1000);
+		
+		aP.sendKeysEndOfEventEditText("11.02.2026");
+		Thread.sleep(1000);		
+		
+		aP.sendKeysTimeOfEventEditText("11:00");
+		Thread.sleep(1000);
+		
+		aP.clickTimeOfEventEditText();
+		Thread.sleep(1000);
+		
+		aP.clickPopUpOkButton();
+		Thread.sleep(1000);
+		
+		aP.sendKeysEndTimeOfEventEditText("11.30");
+		Thread.sleep(1000);
+		
+		aP.clickAddEventPageSaveButton();
 		Thread.sleep(5000);
+		
 	}
 
 }
